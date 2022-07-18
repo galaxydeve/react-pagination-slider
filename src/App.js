9602f1f6de7837caac447bf9f42859cdb5b54e60
galaxydeve <devphs513@gmail.com>
2022-07-19 07:18:19 +0800

    slider done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import { motion } from 'framer-motion';
+import images from './assets'
+import { useEffect, useRef, useState } from 'react';
+import { Pagination } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles({
+  pagination: {
+    width: '100%',
+    '& .MuiPagination-ul li:first-child': {
+      margin: '0 auto 0 0'
+    },
+    '& .MuiPagination-ul li:last-child': {
+      margin: '0 0 0 auto'
+    }
+  }
+})
 
 function App() {
+  const classes = useStyles();
+  const [width, setWidth] = useState(0);
+  const carousel = useRef();
+  useEffect(() => {
+    console.log(carousel.current.scrollWidth, carousel.current.offsetWidth)
+    setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth)
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ width: '800px', height: '400px' }}>
+        <motion.div className='carousel' ref={carousel} whileTap={{ cursor: 'grabbing' }}>
+          <motion.div
+            drag='x'
+            dragConstraints={{ right: 0, left: -width }}
+            className='inner-carousel'>
+            {images.map((image) => {
+              return (
+                <motion.div className='item'>
+                  <img src={image} alt='img' key={image} />
+                </motion.div>
+              );
+            })
+            }
+          </motion.div>
+        </motion.div>
+
+        <Pagination
+          className={classes.pagination}
+          count={Math.round(images.length / 3)}
+          shape='rounded'
+        />
+      </div>
     </div>
   );
 }
---
+++ src/assets/index.js
@@ -0,0 +1,17 @@
+import image1 from './images/1.jpg';
+import image2 from './images/2.jpg';
+import image3 from './images/3.jpg';
+import image4 from './images/4.jpg';
+import image5 from './images/5.jpg';
+import image6 from './images/6.jpg';
+import image7 from './images/1.jpg';
+import image8 from './images/2.jpg';
+import image9 from './images/3.jpg';
+import image10 from './images/4.jpg';
+import image11 from './images/5.jpg';
+import image12 from './images/6.jpg';
+
+export default [
+    image1, image2, image3, image4, image5, image6,
+    image7, image8, image9, image10, image11,
+];
